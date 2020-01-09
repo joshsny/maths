65b25e923f97ada8e4d9a5f30d68e6eccb6ea873
Joshua Snyder <js@joshsny.com>
2020-01-09 16:14:10 +0000

    RT_ES3 creation

---
+++ Example Sheets/latex/auto/RT_ES3.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "RT_ES3"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "header"
+    "titlepage"
+    "article"
+    "art10"
+    "tkz-graph")
+   (TeX-add-symbols
+    "shadow"
+    "npart"
+    "ntitle"
+    "nsheet"
+    "ndate"
+    "SO"))
+ :latex)
+
